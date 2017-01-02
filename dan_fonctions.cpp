0a14ee2f31ca76738ace5f4f3c9df31ae8333cac
Dan-RT <daniel.regnard@efrei.net>
2017-01-02 18:08:55 +0100

    Lots of improvement, rank computation almost done

---
+++ dan_fonctions.cpp
@@ -0,0 +1,9 @@
+//
+//  dan_fonctions.cpp
+//  Projet Graphes
+//
+//  Created by Daniel Regnard on 01/01/2017.
+//  Copyright © 2017 A. All rights reserved.
+//
+
+#include "dan_fonctions.hpp"
---
+++ dan_fonctions.hpp
@@ -0,0 +1,14 @@
+//
+//  dan_fonctions.hpp
+//  Projet Graphes
+//
+//  Created by Daniel Regnard on 01/01/2017.
+//  Copyright © 2017 A. All rights reserved.
+//
+
+#ifndef dan_fonctions_hpp
+#define dan_fonctions_hpp
+
+#include <stdio.h>
+
+#endif /* dan_fonctions_hpp */
